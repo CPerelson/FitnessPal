ebd097875783a0ce4635dfddc8c9ad8760f0508e
scousin2 <126409148+scousin2@users.noreply.github.com>
2024-02-29 22:24:29 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
