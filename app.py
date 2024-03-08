44bd08bb48cc8f78e6b59e27086d747115c71e45
Sara Cousin <saracousin@Saras-MacBook-Air.local>
2024-03-07 21:55:37 -0500

    testing

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Sara'}
     return render_template('index.html', title='Home', user=user)
