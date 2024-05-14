5f92a765347fef65e7d7977eecb6faca57357819
CPerelson <chayaperelson1@gmail.com>
2024-05-14 19:31:46 -0400

    add to init

--- fitness/__init__.py
+++ fitness/__init__.py
@@ -13,11 +13,14 @@ def create_app():
 
     db.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    from .models import User
+    from .models import User    
 
     @login_manager.user_loader
     def load_user(user_id):
