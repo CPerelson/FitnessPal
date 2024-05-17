46a2d4a154c68007c268bf6b904c7340befe6c02
CPerelson <chayaperelson1@gmail.com>
2024-05-16 21:58:05 -0400

    working db!!!

--- fitness/auth.py
+++ fitness/auth.py
@@ -21,7 +21,7 @@ def login_post():
 
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.password, password):
+    if not user or not password:
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
@@ -47,7 +47,7 @@ def signup_post():
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name, password=password)
 
     # add the new user to the database
     db.session.add(new_user)
@@ -55,6 +55,7 @@ def signup_post():
 
     return redirect(url_for('auth.login'))
 
+@auth.route('/logout')
 @login_required
 def logout():
     logout_user()
